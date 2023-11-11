0538bbe386c4a639c6d6ce346ca5d8932c4cae17
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-11-11 23:11:24 +0100

    Creating Models

---
+++ ChatApp.Models/Dtos/MessageUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models.Dtos
+{
+    public class MessageUpdateDto
+    {
+        public int MessageId { get; set; }
+        public string MessageText { get; set; } = string.Empty;
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+
+    }
+
+}
